2285116b7d7bef48b773b6b07b3b70152037cc30
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-09-14 02:34:00 +0430

    fix : setup file dependecies updated

--- setup.py
+++ setup.py
@@ -11,5 +11,8 @@ setup(
   download_url = 'https://github.com/sepandhaghighi/csv2yaml/tarball/v0.1',
   keywords = ['csv', 'json', 'python3','python','yaml',"pickle","moduland"],
   classifiers = [],
+  install_requires=[
+      'pyyaml',
+      ],
   license='MIT',
 )
